7ba4ed6bd19ab1652801d28cf50301f077fd211b
khatia-turm <turmanidzekhatia66@gmail.com>
2026-01-11 23:44:18 +0400

    fixed for deployment

--- server/server.js
+++ server/server.js
@@ -13,13 +13,8 @@ app.use(express.json());
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', '../dist/index.html'));
-});
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.use(express.static(path.join(__dirname, '../dist')));
+
 const db = await JSONFilePreset('db.json', { authUsers: [], employees: [] });
 const DUMMY_TOKEN = 'authorized-can-access';
 function authMiddleware(req, res, next) {
@@ -127,4 +122,12 @@ app.put('/users/:id/role', authMiddleware, async (req, res) => {
   await db.write();
 
   res.json({ message: 'Role updated successfully', employee: employeeToEdit });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
